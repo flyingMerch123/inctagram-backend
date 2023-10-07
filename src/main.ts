0f73c5195fab384e91e60dbda57ab4faeab9a6a3
flyingMerch123 <=>
2023-10-07 18:36:26 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication =
+    await NestFactory.create<INestApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
