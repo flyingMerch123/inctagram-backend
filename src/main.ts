1ab30e779f1e1118e50a1dd0afe5631387de5b4b
flyingMerch123 <=>
2023-10-10 20:16:21 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+const logger: Logger = new Logger('bootstrap');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  const config = app.get<ConfigService>(ConfigService);
+
+  const PORT = config.getOrThrow('PORT', 30000);
+
+  await app.listen(PORT);
+  logger.log(`Server is running  on port: ${PORT}`);
 }
 bootstrap();
